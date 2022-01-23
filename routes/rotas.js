58fc4af584b8629c8c59c709a5b7415373c77c2d
Bruno Fernandes <30004461@students.ual.pt>
2022-01-23 21:59:02 +0000

    rotas concluidas e testes

---
+++ routes/rotas.js
@@ -0,0 +1,79 @@
+module.exports = (app) => {
+  const controlador = require("../controllers/controller");
+
+  const multer = require('multer')
+
+  var router = require("express").Router();
+
+  const fileStorareEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './views/docs' )
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname)
+    },
+  });
+  
+  const upload = multer({storage: fileStorareEngine});
+
+  router.post("/single", upload.single("file"), (req, res) => {
+    console.log(req.file);
+    res.send("File uploaded with sucess");
+  });
+
+  router.post("/multiple", upload.array("files", 3), (req, res) => {
+    console.log(req.file);
+    res.send("Files uploaded with sucess");
+  });
+
+  const path = require("path");
+
+//   router.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname + "/index.html"));
+//   });
+
+//   router.get("/", (req, res) => {
+//     res.render("index");
+//   });
+
+  // Cria um novo utilizador
+  router.post("/registar", controlador.registar);
+
+  // Rota para login - tem de ser POST para não vir user e pass na URL
+  router.post("/login", controlador.login);
+
+  // Rota para verificar e ativar o utilizador
+  router.get("/auth/confirm/:confirmationCode", controlador.verificaUtilizador);
+
+  router.post("/admin", controlador.create);
+
+  router.get("/admin", controlador.consult);
+
+  router.post("/grupo", controlador.createGrupo);
+
+  router.get("/grupos", controlador.consultGrupos);
+
+  router.get("/docentes", controlador.consultDocentes);
+
+  router.post("/questoes", controlador.createQuestoes);
+
+  router.post("/atividades", controlador.createAtividades);
+
+  router.post("/modulos", controlador.createModulos);
+
+  router.get("/questoes", controlador.consultPerguntas);
+
+  router.post("/docentes", controlador.createDocentes);
+
+  router.post("/upload", upload.any(), (req, res) => {
+    console.log(req.files);
+    res.setHeader("Aceess-Control-Allow-Origin", "*");
+    res.send("Done.");
+  });
+
+//   router.get("/", (req, res) => {
+//     res.render;
+//   });
+
+  app.use('/', router);
+};
--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 const PORTA = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
-const multer = require('multer')
 const https = require('https')
 const fs = require('fs')
-app.set('view-engine', 'ejs')
-var router = require("express").Router();
-const controlador = require("../DEVWEV_TBL-main/controllers/controller");
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser())
 app.use(express.json()); // Faz o parse (validação e interpretação) de solicitações do tipo application/json
 app.use(express.urlencoded({ extended: true }));
 
-const fileStorareEngine = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './views/docs' )
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname)
-  },
-});
-
-const upload = multer({storage: fileStorareEngine});
+require('./routes/rotas')(app)
 
 const sslServer = https.createServer({
   key: fs.readFileSync('cert/key.pem'),
   cert:fs.readFileSync('cert/certificate.pem')
 }, app)
 
-const cors = require("cors");
-app.use(cors({
-  exposedHeaders: ['Location'],
-}));
 
 app.use('/assets', express.static('assets'));
 app.use('/views', express.static('views'));
@@ -38,66 +24,4 @@ sslServer.listen(PORTA, () => {
   console.log(`O servidor está a ouvir na porta ${PORTA}`)
 })
 
-app.post('/single', upload.single("file"),(req,res) => {
-  console.log(req.file);
-  res.send('File uploaded with sucess');
-});
-
-app.post('/multiple', upload.array("files", 3),(req,res) => {
-  console.log(req.file);
-  res.send('Files uploaded with sucess');
-});
-
-const path = require('path');
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/', (req,res) => {
-  res.render('index')
-})
-
-
-// Cria um novo utilizador
-app.post("/registar", controlador.registar);
-  
-// Rota para login - tem de ser POST para não vir user e pass na URL
-app.post("/login", controlador.login);
-
-// Rota para verificar e ativar o utilizador
-app.get("/auth/confirm/:confirmationCode", controlador.verificaUtilizador)
-
-app.post("/admin", controlador.create);
-
-app.get("/admin", controlador.consult);
-
-app.post("/grupo", controlador.createGrupo);
-
-app.get("/grupos", controlador.consultGrupos);
-
-app.get("/docentes", controlador.consultDocentes);
-
-app.post("/questoes", controlador.createQuestoes);
-
-app.post("/atividades", controlador.createAtividades);
-
-app.post("/modulos", controlador.createModulos);
-
-app.get("/questoes", controlador.consultPerguntas);
-
-app.post("/docentes", controlador.createDocentes);
-
-
-app.post("/upload", upload.any(), (req, res) => {
-  console.log(req.files)
-  res.setHeader("Aceess-Control-Allow-Origin", "*")
-  res.send("Done.")
-})
-
-app.get('/', (req,res) => {
-  res.render
-})
-
-
-module.exports = app;
+app.use(express.static('public'))
